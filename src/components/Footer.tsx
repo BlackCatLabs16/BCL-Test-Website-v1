f4201b0a2e7863c557d575bc7bec4482cf84f2c6
BlackCatLabs16 <anthan@blackcatlabs.xyz>
2025-02-12 14:06:37 -0500

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -23,19 +23,20 @@ export default function Footer() {
             </ul>
           </div>
           
-          <div>
+          </div>
+          <div className="text-center">
             <h3 className="font-bold text-lg mb-4">Contact</h3>
             <ul className="space-y-2 text-zinc-400">
-              <li className="flex items-center">
-                <Mail className="h-4 w-4 mr-2" />
+              <li className="flex items-center mr-4">
+                <Mail className="h-5 w-5 mr-2" />  {/* Increased icon size */}
                 meow@blackcatlabs.xyz
               </li>
-              <li className="flex items-center">
-                <Phone className="h-4 w-4 mr-2" />
+              <li className="flex items-center mr-4">
+                <Phone className="h-5 w-5 mr-2" />  {/* Increased icon size */}
                 (857) 995-9926
               </li>
-              <li className="flex items-center">
-                <MapPin className="h-4 w-4 mr-2" />
+              <li className="flex items-center text-justify">
+                <MapPin className="h-5 w-5 mr-2" />  {/* Increased icon size */}
                 Black Cat Labs, 47B Webster Avenue, Somerville, MA, 02143, US
               </li>
             </ul>
